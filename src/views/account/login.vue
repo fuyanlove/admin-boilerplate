9a8c43762ea1b4a27a37943b2d8db298c8a25a91
iRuxu <rx6@qq.com>
2022-08-07 23:10:41 +0800

    chore:rm usehead

---
+++ src/views/account/login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="p-login"></div>
+</template>
+
+<script>
+export default {
+    name: "AccountLogin",
+    props: [],
+    components: {},
+    data: function () {
+        return {};
+    },
+    computed: {},
+    watch: {},
+    methods: {},
+    created: function () {},
+    mounted: function () {},
+};
+</script>
